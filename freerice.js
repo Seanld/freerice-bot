640fc306876397c29efc9661530e51396ef7880f
Sean Wilkerson <sean@bucketwonders.com>
2022-02-28 13:42:40 -0700

    Wrapped into Puppeteer (main script not working yet)

--- freerice.js
+++ freerice.js
@@ -3,64 +3,81 @@ const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 
 const TARGET_URL = 'https://freerice.com/categories/basic-math-pre-algebra';
 
-// async function parse(problem) {
-//     return problem.slice(0, -1).replaceAll(' x ', ' * ');
-// }
+function puppeteerScript() {
+    function parse(problem) {
+        return problem.slice(0, -1).replaceAll(' x ', ' * ');
+    }
 
-// async function submit(answer) {
-//     const options = document.querySelectorAll('.card-button');
-//     let flag = false;
-//     for (let i of options) {
-//         if (i.innerHTML == answer) {
-//             i.click();
-//             flag = true;
-//         }
-//     }
-//     if (flag == false) {
-//         option[0].click();
-//     }
-// }
+    function submit(answer) {
+        const options = document.querySelectorAll('.card-button');
+        let flag = false;
+        for (let i of options) {
+            if (i.innerHTML == answer) {
+                i.click();
+                flag = true;
+            }
+        }
+        if (flag == false) {
+            option[0].click();
+        }
+    }
 
-// let counter = 0;
+    let counter = 0;
 
-// async function main() {
-//     try {
-//         const problem = document.querySelector('.card-title').textContent;
-//         const parsedProblem = parse(problem);
-//         const answer = eval(parsedProblem);
-//         submit(answer);
-//         counter++;
-//         console.log(`You've gotten ${counter} right answers!`);
-//     } catch {
-//         document.querySelector('.card-button').click();
-//         console.log('Too hard of a question ;_;');
-//     }
-// }
+    function main() {
+        try {
+            const problem = document.querySelector('.card-title').textContent;
+            const parsedProblem = parse(problem);
+            const answer = eval(parsedProblem);
+            submit(answer);
+            counter++;
+            console.log(`You've gotten ${counter} right answers!`);
+        } catch {
+            document.querySelector('.card-button').click();
+            console.log('Too hard of a question ;_;');
+        }
+    }
 
-// async function mainLoop() {
-//     // Since the delay argument to `setTimeout` needs to change
-//     // every iteration, we can't use `setInterval`. Recursion is
-//     // used instead.
-//     const delay = Math.round((Math.random() * 2) * 1000) + 3000;
-//     setTimeout(() => {
-//         console.clear();
-//         console.log(`${delay} milliseconds of delay`);
-//         main();
-//         // This does not trigger a stack overflow as expected, because
-//         // since `setTimeout` is delegated to the browser to keep track
-//         // of, the previous call is destroyed.
-//         mainLoop();
-//     }, delay)
-// }
+    function mainLoop() {
+        // Since the delay argument to `setTimeout` needs to change
+        // every iteration, we can't use `setInterval`. Recursion is
+        // used instead.
+        const delay = Math.round((Math.random() * 2) * 1000) + 3000;
+        setTimeout(() => {
+            console.clear();
+            console.log(`${delay} milliseconds of delay`);
+            main();
+            // This does not trigger a stack overflow as expected, because
+            // since `setTimeout` is delegated to the browser to keep track
+            // of, the previous call is destroyed.
+            mainLoop();
+        }, delay)
+    }
+
+    mainLoop();
+}
 
 // Handles the age prompt upon first visit to page.
 // birthDateStr must be formatted like 'February 28, 2022'.
-async function doAgePrompt(page, birthDateStr) {
-    const inputFieldClass = '.react-datepicker-ignore-onclickoutside'
-    // await page.waitForSelector(inputFieldClass);
-    const inputField = await page.$$(inputFieldClass);
-    console.log(inputField);
-    inputField.value = birthDateStr;
+async function doAgePrompt(page) {
+    await page.evaluate(() => {
+        let inputBox = document.getElementsByTagName('input')[0];
+
+        // Clone the input box, and set values. This removes the event handlers
+        // that override the date we insert, so it can't be changed back once we set it.
+        let inputBoxClone = inputBox.cloneNode(true);
+        inputBoxClone.className = inputBox.className;
+        inputBoxClone.value = 'June 5, 1995';
+
+        // Swap the original input box with the stunted one, that
+        // has our new date inserted into it.
+        let inputBoxParent = inputBox.parentNode;
+        inputBoxParent.replaceChild(inputBoxClone, inputBox);
+
+        // Submit the input.
+        let ageSaveBtn = document.getElementsByClassName('age-screen-save-button')[0];
+        ageSaveBtn.click();
+    });
 }
 
 puppeteer.use(StealthPlugin());
@@ -71,19 +88,23 @@ puppeteer.use(StealthPlugin());
     });
     const page = (await browser.pages())[0];
     await page.goto(TARGET_URL);
-    await page.waitFor(10000);
-    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+    await page.waitFor(4000);
+    // await page.screenshot({ path: 'screenshot.png', fullPage: true });
 
+    // If visiting for the first time, an age prompt screen will appear.
+    // This gives it a random value, and clicks through the prompt.
     let url = await page.url();
     if (url == 'https://freerice.com/age-screen') {
-        await doAgePrompt(page, 'June 12, 2001');
+        await doAgePrompt(page);
     }
 
-    await page.screenshot({ path: 'screenshot2.png', fullPage: true });
+    await page.waitFor(3000);
+
+    // Now run the actual answer automation code.
+    await page.evaluate(puppeteerScript);
 
     await browser.close();
 })();
-// mainLoop()
 
 
 //random time intervals, rare time delays
