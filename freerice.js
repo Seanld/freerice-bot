5dcfeeea673360d2fa385bdc52880747bb9814c2
Sean Wilkerson <sean@bucketwonders.com>
2022-02-28 10:07:21 -0700

    Updating script to async/await, using Puppeteer

--- freerice.js
+++ freerice.js
@@ -1,54 +1,89 @@
-function parse(problem) {
-    return problem.slice(0, -1).replaceAll(' x ', ' * ');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+
+const TARGET_URL = 'https://freerice.com/categories/basic-math-pre-algebra';
+
+// async function parse(problem) {
+//     return problem.slice(0, -1).replaceAll(' x ', ' * ');
+// }
+
+// async function submit(answer) {
+//     const options = document.querySelectorAll('.card-button');
+//     let flag = false;
+//     for (let i of options) {
+//         if (i.innerHTML == answer) {
+//             i.click();
+//             flag = true;
+//         }
+//     }
+//     if (flag == false) {
+//         option[0].click();
+//     }
+// }
+
+// let counter = 0;
+
+// async function main() {
+//     try {
+//         const problem = document.querySelector('.card-title').textContent;
+//         const parsedProblem = parse(problem);
+//         const answer = eval(parsedProblem);
+//         submit(answer);
+//         counter++;
+//         console.log(`You've gotten ${counter} right answers!`);
+//     } catch {
+//         document.querySelector('.card-button').click();
+//         console.log('Too hard of a question ;_;');
+//     }
+// }
+
+// async function mainLoop() {
+//     // Since the delay argument to `setTimeout` needs to change
+//     // every iteration, we can't use `setInterval`. Recursion is
+//     // used instead.
+//     const delay = Math.round((Math.random() * 2) * 1000) + 3000;
+//     setTimeout(() => {
+//         console.clear();
+//         console.log(`${delay} milliseconds of delay`);
+//         main();
+//         // This does not trigger a stack overflow as expected, because
+//         // since `setTimeout` is delegated to the browser to keep track
+//         // of, the previous call is destroyed.
+//         mainLoop();
+//     }, delay)
+// }
+
+// Handles the age prompt upon first visit to page.
+// birthDateStr must be formatted like 'February 28, 2022'.
+async function doAgePrompt(page, birthDateStr) {
+    const inputFieldClass = '.react-datepicker-ignore-onclickoutside'
+    // await page.waitForSelector(inputFieldClass);
+    const inputField = await page.$$(inputFieldClass);
+    console.log(inputField);
+    inputField.value = birthDateStr;
 }
 
-function submit(answer) {
-    const options = document.querySelectorAll('.card-button');
-    let flag = false;
-    for (let i of options) {
-        if (i.innerHTML == answer) {
-            i.click();
-            flag = true;
-        }
+puppeteer.use(StealthPlugin());
+
+(async () => {
+    const browser = await puppeteer.launch({
+        headless: false
+    });
+    const page = (await browser.pages())[0];
+    await page.goto(TARGET_URL);
+    await page.waitFor(10000);
+    await page.screenshot({ path: 'screenshot.png', fullPage: true });
+
+    let url = await page.url();
+    if (url == 'https://freerice.com/age-screen') {
+        await doAgePrompt(page, 'June 12, 2001');
     }
-    if (flag == false) {
-        option[0].click();
-    }
-}
 
-let counter = 0;
+    await page.screenshot({ path: 'screenshot2.png', fullPage: true });
 
-function main() {
-    try {
-        const problem = document.querySelector('.card-title').textContent;
-        const parsedProblem = parse(problem);
-        const answer = eval(parsedProblem);
-        submit(answer);
-        counter++;
-        console.log(`You've gotten ${counter} right answers!`);
-    } catch {
-        document.querySelector('.card-button').click();
-        console.log('Too hard of a question ;_;');
-    }
-}
-
-function mainLoop() {
-    // Since the delay argument to `setTimeout` needs to change
-    // every iteration, we can't use `setInterval`. Recursion is
-    // used instead.
-    const delay = Math.round((Math.random() * 2) * 1000) + 3000;
-    setTimeout(() => {
-        console.clear();
-        console.log(`${delay} milliseconds of delay`);
-        main();
-        // This does not trigger a stack overflow as expected, because
-        // since `setTimeout` is delegated to the browser to keep track
-        // of, the previous call is destroyed.
-        myLoop();
-    }, delay)
-}
-      
-mainLoop()
+    await browser.close();
+})();
+// mainLoop()
 
 
 //random time intervals, rare time delays
